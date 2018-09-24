6787dadffd3918dbd825e750f7cf55d0ecf73b2f
MiniMarker <cfmarker@gmail.com>
2018-09-24 16:53:41 +0200

    added Swagger docu

---
+++ README.md
@@ -0,0 +1,10 @@
+# Pokemon REST API 
+
+[![Build Status](https://travis-ci.org/MiniMarker/PokemonRestApi.svg?branch=master)](https://travis-ci.org/MiniMarker/PokemonRestApi)
+
+URL: https://polar-oasis-62172.herokuapp.com/pokemon
+
+Docs: https://polar-oasis-62172.herokuapp.com/swagger-ui.html
+
+----
+This is a REST API build in Kotlin and Spring
--- src/main/kotlin/no/cmarker/PokemonRestApi/PokemonRestApiApplication.kt
+++ src/main/kotlin/no/cmarker/PokemonRestApi/PokemonRestApiApplication.kt
@@ -1,10 +1,47 @@
 package no.cmarker.PokemonRestApi
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
+import springfox.documentation.builders.ApiInfoBuilder
+import springfox.documentation.builders.PathSelectors
+import springfox.documentation.service.ApiInfo
+import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spring.web.plugins.Docket
+import springfox.documentation.swagger2.annotations.EnableSwagger2
 
 @SpringBootApplication
-class PokemonRestApiApplication
+@EnableJpaRepositories
+@EntityScan
+@EnableSwagger2
+class PokemonRestApiApplication {
+    
+    /*
+        Bean used to configure Swagger documentation
+     */
+    @Bean
+    fun swaggerApi(): Docket {
+        return Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .paths(PathSelectors.any())
+                .build()
+    }
+    
+    private fun apiInfo(): ApiInfo {
+        return ApiInfoBuilder()
+                .title("API for REST Pokemon")
+                .description("This is a REST API containing all pokemons that exists in 1. Gen")
+                .version("1.0")
+                .build()
+    }
+    
+    //http://localhost:8080/pokemon/swagger-ui.html
+    
+    
+}
 
 fun main(args: Array<String>) {
     runApplication<PokemonRestApiApplication>(*args)
